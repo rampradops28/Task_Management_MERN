5b51b496231640d3117d60c7108fa4370707418a
rampradops28 <rampradop28@gmail.com>
2025-06-12 19:28:04 +0530

    sample

--- Backend/server.js
+++ Backend/server.js
@@ -9,12 +9,24 @@ import { connectDB } from "./src/config/dbConfig.js";
 const app = express();
 const httpServer = createServer(app);
 
+console.log("CORS allowed origin:", FRONTEND);
 // CORS configuration
+const allowedOrigins = [
+  "https://task-management-mern-amber.vercel.app",
+  "http://localhost:5173"
+];
+
 app.use(cors({
-    origin: FRONTEND || "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-access-token"]
 }));
 
 // Middleware
